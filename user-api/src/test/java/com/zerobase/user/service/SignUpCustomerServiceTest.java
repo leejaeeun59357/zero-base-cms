76f342a4bdb82bc1cf9689cb96f6f2cd6bca3278
jaeeun <ne2227@naver.com>
2024-02-17 20:02:03 +0900

    feat : [002-SIGN-UP] 커스터머(고객)에 대한 Signup Service 구현

---
+++ user-api/src/test/java/com/zerobase/user/service/SignUpCustomerServiceTest.java
@@ -0,0 +1,35 @@
+package com.zerobase.user.service;
+
+import com.zerobase.user.domain.SignUpForm;
+import com.zerobase.user.domain.model.Customer;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class SignUpCustomerServiceTest {
+
+    @Autowired
+    private SignUpCustomerService service;
+
+    @Test
+    void signUp() {
+        SignUpForm form = SignUpForm.builder()
+                .name("name")
+                .birth(LocalDate.now())
+                .email("abc@naver.com")
+                .password("1")
+                .phone("01012345678")
+                .build();
+
+        Customer c= service.signUp(form);
+
+        assertNotNull(c.getId());
+        assertNotNull(c.getCreatedAt());
+
+    }
+}
