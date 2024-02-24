110d76d7fc3fda52d961ca50649b4534a979aeb6
jaeeun <ne2227@naver.com>
2024-02-24 22:33:55 +0900

    feat : [010-PRODUCT-MODIFY] Product & ProductItem Modify
    Product & ProductItem Modify 구현

--- order-api/src/main/java/com/zerobase/cms/order/domain/repository/ProductRepository.java
+++ order-api/src/main/java/com/zerobase/cms/order/domain/repository/ProductRepository.java
@@ -4,6 +4,7 @@ import com.zerobase.cms.order.domain.model.Product;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
@@ -12,5 +13,4 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
 
     @EntityGraph(attributePaths = {"productItems"}, type = EntityGraph.EntityGraphType.LOAD)
     Optional<Product> findWithProductItemsById(Long id);
-
 }
